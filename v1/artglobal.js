35e0bf7c4c9e46db180ebc891bde52d972212b5a
Paul Scanlon <pauliescanlon@gmail.com>
2023-04-25 16:05:27 +0100

    feat: add local and global endpoints

---
+++ v1/artglobal.js
@@ -0,0 +1,45 @@
+const { getDB } = require('../pg');
+
+module.exports.handler = async (event) => {
+  const client = await getDB().connect();
+
+  const { user_id, username, values } = JSON.parse(event.body);
+
+  const last_updated = new Date();
+
+  try {
+    const response = await client.query(
+      'UPSERT INTO art_global (user_id, username, last_updated, values) VALUES($1, $2, $3, $4) RETURNING values',
+      [user_id, username, last_updated, values]
+    );
+
+    if (!response.rows) {
+      throw new Error('Bad Response');
+    }
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(
+        {
+          message: 'Art Global v1 - A Ok!',
+        },
+        null,
+        2
+      ),
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify(
+        {
+          message: 'Art Global - Error!',
+          error: error.message,
+        },
+        null,
+        2
+      ),
+    };
+  } finally {
+    client.release();
+  }
+};
---
+++ v1/artlocal.js
@@ -0,0 +1,45 @@
+const { getDB } = require('../pg');
+
+module.exports.handler = async (event) => {
+  const client = await getDB().connect();
+
+  const { user_id, username, region, values } = JSON.parse(event.body);
+
+  const last_updated = new Date();
+
+  try {
+    const response = await client.query(
+      'UPSERT INTO art_local (user_id, username, last_updated, region, values) VALUES($1, $2, $3, $4, $5) RETURNING values',
+      [user_id, username, last_updated, `aws-${region}`, values]
+    );
+
+    if (!response.rows) {
+      throw new Error('Bad Response');
+    }
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(
+        {
+          message: 'Art Local v1 - A Ok!',
+        },
+        null,
+        2
+      ),
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify(
+        {
+          message: 'Art Local - Error!',
+          error: error.message,
+        },
+        null,
+        2
+      ),
+    };
+  } finally {
+    client.release();
+  }
+};
