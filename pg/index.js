c2033b7748551160f45589c749184bf4a357ec8a
Paul Scanlon <pauliescanlon@gmail.com>
2023-04-27 20:36:29 +0100

    chore: add debug true to serverless-postrgres

--- pg/index.js
+++ pg/index.js
@@ -2,7 +2,7 @@ const ServerlessClient = require('serverless-postgres');
 
 const connectionString = process.env.DATABASE_URL;
 
-const client = new ServerlessClient({ connectionString });
+const client = new ServerlessClient({ connectionString, debug: true });
 
 module.exports = {
   client,
