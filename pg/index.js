ed56ce958a4f9400c34376f5de8607ac5276b034
Paul Scanlon <pauliescanlon@gmail.com>
2023-04-28 10:43:39 +0100

    refactor: add config to ServerlessClient

--- pg/index.js
+++ pg/index.js
@@ -2,7 +2,13 @@ const ServerlessClient = require('serverless-postgres');
 
 const connectionString = process.env.DATABASE_URL;
 
-const client = new ServerlessClient({ connectionString, debug: true });
+const client = new ServerlessClient({
+  application_name: 'demo-data-residency',
+  connectionString,
+  strategy: 'minimum_idle_time',
+  maxConnections: 1000,
+  debug: true,
+});
 
 module.exports = {
   client,
