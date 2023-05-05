5bfb18bb69e20ddde0dcc10e871996d13cde2ed1
Paul Scanlon <pauliescanlon@gmail.com>
2023-05-05 20:27:28 +0100

    test: add fast-geoip

--- v1/index.js
+++ v1/index.js
@@ -1,6 +1,10 @@
 const { headers } = require('../utils');
+const geoip = require('fast-geoip');
 
 module.exports.handler = async (event, context) => {
+  const ip = event.requestContext.identity.sourceIp;
+  const geo = await geoip.lookup(ip);
+
   return {
     statusCode: 200,
     headers: headers,
@@ -8,6 +12,7 @@ module.exports.handler = async (event, context) => {
       message: 'Index v1 - A Ok!',
       region: process.env.AWS_REGION,
       cockroach_region: `aws-${process.env.AWS_REGION}`,
+      geo,
     }),
   };
 };
